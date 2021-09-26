c22571d58687efa30e23402c201b1c9bded85bbe
Dong <0x317374@gmail.com>
2021-09-27 04:36:27 +0700

    chore: happy work

---
+++ scripts/build.ts
@@ -0,0 +1,67 @@
+import pug = require("pug");
+import path = require("path");
+import { getAllDirs, getAllFiles } from "../utils/utils";
+import fs = require("fs-extra");
+import yaml = require("yaml");
+
+async function main() {
+  const workingDir = path.join(__dirname, "../.");
+  const assetsDir = path.join(__dirname, "../assets");
+  const pagesDir = path.join(__dirname, "../pages");
+  const distDir = path.join(__dirname, "../dist");
+  const assets = await getAllDirs(assetsDir);
+  const pageFiles = await getAllFiles(pagesDir);
+  const processed: {
+    [key: string]: boolean;
+  } = {};
+
+  await fs.remove(distDir);
+
+  for (const filename of pageFiles) {
+    const project = path.dirname(filename).split(path.sep).pop();
+
+    if (!processed[project]) {
+      const project = path.dirname(filename).split(path.sep).pop();
+
+      for (const asset of assets) {
+        if (!(await fs.lstat(asset)).isDirectory()) return;
+
+        if (asset.split(path.sep).pop() === project) {
+          const parent = path.dirname(asset);
+          const to = parent.replace(workingDir, "");
+          const target = path.join(distDir, project, to);
+
+          await fs.copy(asset, target);
+
+          console.log(project, asset.replace(workingDir, ""), "->", target.replace(workingDir, ""));
+        }
+      }
+
+      processed[project] = true;
+    }
+
+    if (!filename.endsWith(".pug")) return;
+
+    const chunks: Array<string> = filename.split(".pug");
+
+    chunks.pop();
+
+    const saveTo = (chunks.join(".pug") + ".html").replace(pagesDir, distDir);
+    const dataFile = chunks.join(".pug") + ".yaml";
+    let locals = {};
+
+    if (fs.existsSync(dataFile)) {
+      locals = yaml.parse(await fs.readFile(dataFile, "utf-8"));
+    }
+
+    const fn = pug.compileFile(filename);
+    const html = fn(locals);
+
+    await fs.ensureFile(saveTo);
+    await fs.writeFile(saveTo, html);
+
+    console.log(project, filename.replace(workingDir, ""), "->", saveTo.replace(workingDir, ""));
+  }
+}
+
+main().catch(console.error);
--- utils/utils.ts
+++ utils/utils.ts
@@ -1,22 +1,36 @@
 import path = require("path");
-import _ = require("lodash");
 import fs = require("fs-extra");
 
 export async function getAllFiles(dir: string): Promise<Array<string>> {
   let result: Array<string> = [];
   const files = await fs.readdir(dir);
 
-  await Promise.all(
-    _.map(files, async (filename: string) => {
-      filename = path.join(dir, filename);
+  for (let filename of files) {
+    filename = path.join(dir, filename);
 
-      if ((await fs.lstat(filename)).isDirectory()) {
-        result = result.concat(await getAllFiles(filename));
-      } else {
-        result.push(filename);
-      }
-    })
-  );
+    if ((await fs.lstat(filename)).isDirectory()) {
+      result = result.concat(await getAllFiles(filename));
+    } else {
+      result.push(filename);
+    }
+  }
+
+  return result;
+}
+
+export async function getAllDirs(dir: string): Promise<Array<string>> {
+  let result: Array<string> = [];
+  const dirs = await fs.readdir(dir);
+
+  for (let dirname of dirs) {
+    dirname = path.join(dir, dirname);
+
+    if ((await fs.lstat(dirname)).isDirectory()) {
+      result.push(dirname);
+
+      result = result.concat(await getAllDirs(dirname));
+    }
+  }
 
   return result;
 }
