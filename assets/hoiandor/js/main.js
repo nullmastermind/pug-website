2d7d49c557f39ad3fb4a5dff0123e748c72a2154
Dong <0x317374@gmail.com>
2021-09-27 12:07:27 +0700

    chore: happy work

--- assets/hoiandor/js/main.js
+++ assets/hoiandor/js/main.js
@@ -7,4 +7,20 @@ $(document).ready(function () {
       return image.src.replace("-cropped.", ".");
     },
   });
+
+  // var sameHeightClasses = [".la-same-height-01"];
+  //
+  // for (var i = 0; i < sameHeightClasses.length; i++) {
+  //   var maxHeight = -1;
+  //
+  //   $(sameHeightClasses[i]).each(function (index) {
+  //     var height = $(this).height();
+  //
+  //     if (height > maxHeight) maxHeight = height;
+  //   });
+  //
+  //   if (maxHeight > 0) {
+  //     $(sameHeightClasses[i]).css("height", maxHeight + 22 + "px");
+  //   }
+  // }
 });
